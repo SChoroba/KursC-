a12aae7d291a82e7253e09049290014fe452f37c
SChoroba <Szymon.Choroba@zspm.edu.pl>
2025-12-07 09:45:56 +0100

    Update petle.cpp

--- petle.cpp
+++ petle.cpp
@@ -61,6 +61,90 @@ int main()
     }
     cout << zoma;
 
-    cout << rev * zoma;
+
+    cout << "=====================================" << endl;
+
+    cout << "z czego wypisać tabliczkę" << endl;
+    int dotabliczki;
+    cin >> dotabliczki;
+
+    for (int i = 0; i<11 ; i++) {
+        cout << dotabliczki << "*" << i << "=" << (dotabliczki * i) << endl;
+    }
+    return 0;
+}
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i < 11; i++) {
+        cout << i<<endl;
+   }
+
+    cout << "=====================================" << endl;
+
+    for (int p = 1; p <= 20; p++) {
+        if (p % 2 == 0) {
+            cout << p<<endl;
+        }
+
+    }
+
+    cout << "=====================================" << endl;
+
+    for (int r = 100; r >= 0; r--) {
+        cout << r << " " << endl;
+    }
+    // od 1 do 100
+    cout << "=====================================" << endl;
+
+    for (int l = 1, suma = 0; l <= 100; l++) {
+        suma += l;
+        cout << suma << " " << endl;
+    }
+
+    cout << "=====================================" << endl;
+
+    for (int k = 1; k < 10; k++) {
+        cout << k * k << endl;
+    }
+
+    int cyfra = 1;
+    do {
+        cout << "Zgadnij cyfrę";
+        cin >> cyfra;
+
+    } while (cyfra != 0);
+    cout << "Podaj silnie do obliczenia przez program: ";
+    int silna_uzytkownika;
+    cin >> silna_uzytkownika;
+    long long wynik = 1;
+    for (int silna = 1;  silna <= silna_uzytkownika; silna++) {
+        wynik *= silna;
+
+    }
+
+    cout << wynik << endl;
+    cout << "=====================================" << endl;
+    int zoma = 0;
+    for (int odjencie = 4; odjencie > 0; odjencie--) {
+        zoma = zoma + odjencie;
+
+    }
+    cout << zoma;
+
+
+    cout << "=====================================" << endl;
+
+    cout << "z czego wypisać tabliczkę" << endl;
+    int dotabliczki;
+    cin >> dotabliczki;
+
+    for (int i = 0; i<11 ; i++) {
+        cout << dotabliczki << "*" << i << "=" << (dotabliczki * i) << endl;
+    }
     return 0;
 }
